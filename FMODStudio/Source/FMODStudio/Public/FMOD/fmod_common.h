8dd72e890cc6cc7b7dbc660d6981a61e4f350b38
fmod-cameron <cameron@fmod.com>
2017-04-20 09:16:28 +1000

    Update to release 1.08.18

--- FMODStudio/Source/FMODStudio/Public/FMOD/fmod_common.h
+++ FMODStudio/Source/FMODStudio/Public/FMOD/fmod_common.h
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00010817
+#define FMOD_VERSION    0x00010818
 
 /*
     Compiler specific settings.
