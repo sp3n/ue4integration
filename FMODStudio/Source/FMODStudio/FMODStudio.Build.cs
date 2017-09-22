d6b7c071278844c52a1d07ca203230b62edd86e0
fmod-cameron <cameron@fmod.com>
2017-09-22 14:16:30 +1000

    Update to release 1.09.08

--- FMODStudio/Source/FMODStudio/FMODStudio.Build.cs
+++ FMODStudio/Source/FMODStudio/FMODStudio.Build.cs
@@ -98,6 +98,13 @@ namespace UnrealBuildTool.Rules
                 libPrefix = "lib";
                 bAddRuntimeDependencies = false;
             }
+            else if (Target.Platform.ToString() == "UWP64")
+            {
+                platformMidName = "_X64";
+                linkExtension = ".lib";
+                dllExtension = ".dll";
+                bAddDelayLoad = true;
+            }
             else
             {
                 switch (Target.Platform)
@@ -117,7 +124,6 @@ namespace UnrealBuildTool.Rules
                         linkExtension = dllExtension = ".dylib";
                         libPrefix = "lib";
                         bLinkFromBinaries = false;
-
                         break;
                     case UnrealTargetPlatform.XboxOne:
                         linkExtension = "_vc.lib";
